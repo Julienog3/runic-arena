0bf5b2bae5dc8e4a0a62ca34582b7edb9100cdb0
Julien Auger <jauger@amiltone.com>
2023-04-25 21:42:36 +0200

    :sparkles: Adding routing logic

---
+++ src/components/AppNavbar.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import NavButton from './NavButton.vue';
+
+
+
+</script>
+
+<template>
+  <nav class="w-48 h-screen bg-neutral-900">
+    <NavButton path="/">Home</NavButton>
+    <NavButton path="/about">About</NavButton>
+  </nav>
+</template>
+
+<style scoped>
+
+</style>
+
---
+++ src/components/NavButton.vue
@@ -0,0 +1,24 @@
+
+
+<script setup lang="ts">
+  import { RouterLink } from 'vue-router'
+
+  interface NavButtonProps {
+    path: string
+  }
+
+  const props = defineProps<NavButtonProps>()
+
+  console.log(props)
+
+</script>
+
+<template>
+  <RouterLink :to="props.path">
+    <span>
+      <slot></slot>
+    </span>
+  </RouterLink>
+</template>
+
+
