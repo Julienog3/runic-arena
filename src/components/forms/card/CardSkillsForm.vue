13f17c983a8960f97a5d96fd5e9e192a758e2bb7
Julien Auger <julien.auger37@laposte.net>
2023-05-10 01:16:40 +0200

    Ading skills dropdown

--- src/components/forms/card/CardSkillsForm.vue
+++ src/components/forms/card/CardSkillsForm.vue
@@ -1,11 +1,19 @@
 <script setup lang="ts">
-import { TypeEnum, type CardType, CategoryEnum } from '@/types/card';
-import type { SkillType } from '@/types/skill';
-import { reactive } from 'vue';
+import SkillsDropdown from '@/components/modals/adding-modal/SkillsDropdown.vue';
+import { SkillTypeEnum, type SkillType } from '@/types/skill';
+import { ref } from 'vue';
 
 interface CardSkillsFormProps {
   skills: SkillType[]
 }
+
+const activeSkills = ref<SkillType[]>([
+  {
+    name: "Critique",
+    description: "Double les dommages des X prochaines attaques",
+    type: SkillTypeEnum.ACTIVE,
+  },
+])
 
 
 const props = defineProps<CardSkillsFormProps>()
@@ -15,8 +23,9 @@ const props = defineProps<CardSkillsFormProps>()
   <form class="absolute top-0 left-0  w-full flex flex-col items-start gap-4">
     <div class="flex flex-col justify-between w-full gap-4">
       <h2 class="text-lg font-semibold text-neutral-800">Capacités actives</h2>
-      <div class="flex flex-col w-full">
-        <label for="type" class="mb-2">Type</label>
+      <SkillsDropdown :skills="activeSkills" :selected-skill="" />
+      <div v-for="skill in activeSkills" :key="skill.name" class="flex flex-col w-full">
+        <label for="type" class="mb-2">Capacité n°1</label>
         <select 
           type="text" 
           id="type"
@@ -25,7 +34,8 @@ const props = defineProps<CardSkillsFormProps>()
           <option value="chaos" selected>Chaos</option>
           <option value="halo">Halo</option>
         </select>
-      </div>
+      </div>d
+      <span @click="" class="text-violet-500 font-semibold cursor-pointer">+ Ajouter une capacité</span>
       <div class="flex flex-col w-full">
         <h2 class="text-lg font-semibold text-neutral-800">Capacité passive</h2>
         <label for="type" class="mb-2">Type</label>
---
+++ src/components/modals/adding-modal/SkillsDropdown.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import type { SkillType } from '@/types/skill';
+
+  interface SkillsDropdownProps {
+    skills: SkillType[]
+    selectedSkill: SkillType
+  }
+
+
+  const props = defineProps<SkillsDropdownProps>()
+</script>
+
+<template>
+  <div class="flex p">
+
+  </div>
+  <ul>
+    <li v-for="skill in skills" :key="skill.name">test</li>
+  </ul>
+</template>
