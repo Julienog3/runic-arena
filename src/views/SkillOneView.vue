ada40498b2bc1d1be44f71d55ad9664de1b2792f
Romain <romain@shokola.com>
2023-05-10 15:16:28 +0200

    add edit skill

--- src/views/SkillOneView.vue
+++ src/views/SkillOneView.vue
@@ -40,19 +40,19 @@ import { useRoute } from 'vue-router';
           <div class="grid gap-6 mb-6 md:grid-cols-3">
               <div>
                   <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-dark">Nom</label>
-                  <input type="text" v-model="skill.name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Le nom" required>
+                  <input type="text" v-model="skill.name" class="w-full border border-neutral-200 p-3 rounded-lg focus:border-violet-500 focus-within:border-violet-500 focus-visible:border-violet-500" placeholder="Le nom" required>
               </div>
           </div>
           <div class="grid gap-6 mb-6 md:grid-cols-3">
               <div>
                   <label for="description" class="block mb-2 text-sm font-medium text-gray-900 dark:text-dark">Description</label>
-                  <input type="message" v-model="skill.description" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="La description">
+                  <input type="message" v-model="skill.description" class="w-full border border-neutral-200 p-3 rounded-lg focus:border-violet-500 focus-within:border-violet-500 focus-visible:border-violet-500" placeholder="La description">
               </div>
           </div>
           <div class="grid gap-6 mb-6 md:grid-cols-3"> 
             <div>
               <label for="valeur" class="block mb-2 text-sm font-medium text-gray-900 dark:text-dark">Valeur</label>
-              <input type="number" v-model="skill.value" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="La valeur" required>
+              <input type="number" v-model="skill.value" class="w-full border border-neutral-200 p-3 rounded-lg focus:border-violet-500 focus-within:border-violet-500 focus-visible:border-violet-500" placeholder="La valeur" required>
             </div>      
           </div>
           <div class="grid gap-6 mb-6 md:grid-cols-3"> 
@@ -64,15 +64,15 @@ import { useRoute } from 'vue-router';
           <div class="grid gap-6 mb-6 md:grid-cols-9">
               <div>
                   <label for="runic" class="block mb-2 text-sm font-medium text-gray-900 dark:text-dark">Coup en runique rouge</label>
-                  <input type="number" v-model="skill.red" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Coup en runique rouge">
+                  <input type="number" v-model="skill.red" class="w-full border border-neutral-200 p-3 rounded-lg focus:border-violet-500 focus-within:border-violet-500 focus-visible:border-violet-500" placeholder="Coup en runique rouge">
               </div>
               <div>
                   <label for="runic" class="block mb-2 text-sm font-medium text-gray-900 dark:text-dark">Coup en runique bleue</label>
-                  <input type="number" v-model="skill.blue" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Coup en runique bleue">
+                  <input type="number" v-model="skill.blue" class="w-full border border-neutral-200 p-3 rounded-lg focus:border-violet-500 focus-within:border-violet-500 focus-visible:border-violet-500" placeholder="Coup en runique bleue">
               </div>
               <div>
                   <label for="runic" class="block mb-2 text-sm font-medium text-gray-900 dark:text-dark">Coup en runique Jaune</label>
-                  <input type="number" v-model="skill.yellow" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Coup en runique jaune">
+                  <input type="number" v-model="skill.yellow" class="w-full border border-neutral-200 p-3 rounded-lg focus:border-violet-500 focus-within:border-violet-500 focus-visible:border-violet-500" placeholder="Coup en runique jaune">
               </div>
           </div>
           <div class="grid gap-6 mb-6 md:grid-cols-3"> 
@@ -81,7 +81,7 @@ import { useRoute } from 'vue-router';
                 <label for="actif" class="ml-2 text-sm font-medium text-dark-900 dark:text-dark-300">La capacité est active</label>
             </div>       
           </div>
-          <button @click="editSkill(+id, skill)" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Modifier</button>
+          <button @click="editSkill(+id, skill)" class="bg-violet-600 font-medium text-white text-md px-12 py-3 rounded-lg">Modifier</button>
       </form>
     </div>
   </main>
